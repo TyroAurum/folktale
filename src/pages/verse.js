3916a6d08139055c429c23f39489853d4cc9a968
TyroAurum <thangaakilan112@gmail.com>
2024-04-09 19:33:58 +0530

    title update 1

--- src/pages/verse.js
+++ src/pages/verse.js
@@ -5,13 +5,13 @@ import SearchBar from "../component/searchbar";
 import TitleBar from "../component/Titlebar";
 import './verse.css';
 
-const datas = [{'link':'https://pixabay.com/photos/holi-girl-indian-india-dance-fun-2416686/','title':'India'},
-            {'link':'https://pixabay.com/photos/china-antiquity-maiden-1572774/','title':'China'},
-            {'link':'https://pixabay.com/photos/kimono-woman-umbrella-parasol-1822520/','title':'Japan'},
-            {'link':'https://pixabay.com/photos/france-lake-mountain-water-nature-4865938/','title':'France'},
-            {'link':'https://pixabay.com/photos/berlin-pantomime-action-artist-4928453/','title':'Germany'},
-            {'link':'https://pixabay.com/photos/musician-guitarist-streets-people-1284394/','title':'Poland'},
-            {'link':'https://pixabay.com/photos/party-parade-people-spain-7767054/','title':'Spain'},
+const datas = [{'link':'https://cdn.pixabay.com/photo/2017/06/18/18/26/holi-2416686_1280.jpg','title':'India'},
+            {'link':'https://cdn.pixabay.com/photo/2016/08/05/17/43/china-1572774_1280.jpg','title':'China'},
+            {'link':'https://cdn.pixabay.com/photo/2016/11/14/03/43/kimono-1822520_1280.jpg','title':'Japan'},
+            {'link':'https://cdn.pixabay.com/photo/2020/02/20/21/15/france-4865938_1280.jpg','title':'France'},
+            {'link':'https://cdn.pixabay.com/photo/2020/03/13/16/29/berlin-4928453_1280.jpg','title':'Germany'},
+            {'link':'https://cdn.pixabay.com/photo/2016/03/27/21/44/musician-1284394_1280.jpg','title':'Poland'},
+            {'link':'https://cdn.pixabay.com/photo/2023/02/04/12/22/party-7767054_1280.jpg','title':'Spain'},
             // {'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz2vsqpqMG-LPUdN6idO0sipjhMxYklHnE7ENjC8BQZgNCt3SIqvW7WJDRX86ni3DxnXY&usqp=CAU','title':'Israel'},
             // {'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz2vsqpqMG-LPUdN6idO0sipjhMxYklHnE7ENjC8BQZgNCt3SIqvW7WJDRX86ni3DxnXY&usqp=CAU','title':'Turkey'},
             // {'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz2vsqpqMG-LPUdN6idO0sipjhMxYklHnE7ENjC8BQZgNCt3SIqvW7WJDRX86ni3DxnXY&usqp=CAU','title':'Somalia'},
