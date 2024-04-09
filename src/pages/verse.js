1787fe01678737f4c76c62ff8832d9dd80ef091e
TyroAurum <thangaakilan112@gmail.com>
2024-04-09 19:28:09 +0530

    image update

--- src/pages/verse.js
+++ src/pages/verse.js
@@ -5,16 +5,16 @@ import SearchBar from "../component/searchbar";
 import TitleBar from "../component/Titlebar";
 import './verse.css';
 
-const datas = [{'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz2vsqpqMG-LPUdN6idO0sipjhMxYklHnE7ENjC8BQZgNCt3SIqvW7WJDRX86ni3DxnXY&usqp=CAU','title':'India'},
-            {'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz2vsqpqMG-LPUdN6idO0sipjhMxYklHnE7ENjC8BQZgNCt3SIqvW7WJDRX86ni3DxnXY&usqp=CAU','title':'China'},
-            {'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz2vsqpqMG-LPUdN6idO0sipjhMxYklHnE7ENjC8BQZgNCt3SIqvW7WJDRX86ni3DxnXY&usqp=CAU','title':'Japan'},
-            {'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz2vsqpqMG-LPUdN6idO0sipjhMxYklHnE7ENjC8BQZgNCt3SIqvW7WJDRX86ni3DxnXY&usqp=CAU','title':'France'},
-            {'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz2vsqpqMG-LPUdN6idO0sipjhMxYklHnE7ENjC8BQZgNCt3SIqvW7WJDRX86ni3DxnXY&usqp=CAU','title':'Germany'},
-            {'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz2vsqpqMG-LPUdN6idO0sipjhMxYklHnE7ENjC8BQZgNCt3SIqvW7WJDRX86ni3DxnXY&usqp=CAU','title':'Poland'},
-            {'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz2vsqpqMG-LPUdN6idO0sipjhMxYklHnE7ENjC8BQZgNCt3SIqvW7WJDRX86ni3DxnXY&usqp=CAU','title':'Spain'},
-            {'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz2vsqpqMG-LPUdN6idO0sipjhMxYklHnE7ENjC8BQZgNCt3SIqvW7WJDRX86ni3DxnXY&usqp=CAU','title':'Israel'},
-            {'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz2vsqpqMG-LPUdN6idO0sipjhMxYklHnE7ENjC8BQZgNCt3SIqvW7WJDRX86ni3DxnXY&usqp=CAU','title':'Turkey'},
-            {'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz2vsqpqMG-LPUdN6idO0sipjhMxYklHnE7ENjC8BQZgNCt3SIqvW7WJDRX86ni3DxnXY&usqp=CAU','title':'Somalia'},
+const datas = [{'link':'https://pixabay.com/photos/holi-girl-indian-india-dance-fun-2416686/','title':'India'},
+            {'link':'https://pixabay.com/photos/china-antiquity-maiden-1572774/','title':'China'},
+            {'link':'https://pixabay.com/photos/kimono-woman-umbrella-parasol-1822520/','title':'Japan'},
+            {'link':'https://pixabay.com/photos/france-lake-mountain-water-nature-4865938/','title':'France'},
+            {'link':'https://pixabay.com/photos/berlin-pantomime-action-artist-4928453/','title':'Germany'},
+            {'link':'https://pixabay.com/photos/musician-guitarist-streets-people-1284394/','title':'Poland'},
+            {'link':'https://pixabay.com/photos/party-parade-people-spain-7767054/','title':'Spain'},
+            // {'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz2vsqpqMG-LPUdN6idO0sipjhMxYklHnE7ENjC8BQZgNCt3SIqvW7WJDRX86ni3DxnXY&usqp=CAU','title':'Israel'},
+            // {'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz2vsqpqMG-LPUdN6idO0sipjhMxYklHnE7ENjC8BQZgNCt3SIqvW7WJDRX86ni3DxnXY&usqp=CAU','title':'Turkey'},
+            // {'link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz2vsqpqMG-LPUdN6idO0sipjhMxYklHnE7ENjC8BQZgNCt3SIqvW7WJDRX86ni3DxnXY&usqp=CAU','title':'Somalia'},
 ]
 
 
